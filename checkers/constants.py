b490c7b103900c4d55c06457e9a516207281e01c
bartoszbereski <101666686+bartoszbereski@users.noreply.github.com>
2023-04-19 19:34:01 +0200

    added game components

---
+++ checkers/constants.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+
+WIDTH, HEIGHT = 800,800
+ROWS, COLS = 8,8
+SQUARE_SIZE = WIDTH//COLS
+
+
+#colors
+RED = (178, 34, 34)
+WHITE = (220,220,220)
+BLACK = (0,0,0)
+BLUE = 	(34,139,34)
+GREY = (128,128,128)
+ECRU = (166,171,181)
+
+BROWN = (210,180,140)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
